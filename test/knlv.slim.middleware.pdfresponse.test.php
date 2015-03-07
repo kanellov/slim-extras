2fe6a483d11dfb91e88953bc8acd50b09bc00716
kanellov <contact@kanellov.com>
2015-03-07 13:09:44 +0200

    added failing test for PdfResponse middleware

---
+++ test/knlv.slim.middleware.pdfresponse.test.php
@@ -0,0 +1,9 @@
+<?php
+require __DIR__ . '/../vendor/autoload.php';
+
+use FUnit as fu;
+use Knlv\Slim\Middleware\PdfResponse;
+
+fu::test('Test PdfResponse middleware', function () {
+    fu::fail('Not Implemented');
+});
